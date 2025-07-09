aee117fbd7f16f5e670bf7430dea8083f842795e
harshit kumar <harshit.page@gmail.com>
2025-07-09 23:19:09 +0530

    -a

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var userModel = require("./users")
 
-router.get('/', (req, res) => {
-  res.render('index');
+router.get('/create', async (req, res) => {
+  try {
+    const createUser = await userModel.create({
+      username: 'harshitheya',
+      age: 21,
+      name: 'harshit'
+    });
+    res.status(201).send(createUser); // Send the created user
+  } catch (err) {
+    res.status(500).send("Error creating user: " + err.message);
+  }
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/nodejs")
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const userschema = mongoose.Schema({
+    username: String, 
+    name: String, 
+    age: Number 
+})
 
-module.exports = router;
+module.exports = mongoose.model('Users',userschema)
